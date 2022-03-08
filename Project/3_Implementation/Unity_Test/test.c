bf6dc26e4e12f46c6b5dfab72ac8168f6f7725bd
Harshitha.N <55775183+Harshitha199928@users.noreply.github.com>
2022-03-08 13:10:21 +0530

    Create test.c

---
+++ Project/3_Implementation/Unity_Test/test.c
@@ -0,0 +1,17 @@
+#include "led.c"
+#include "lcd.c"
+#include "display.c"
+#include "/home/runner/work/
+M2-Embsys/
+M2-Embsys/3_Implementation/Unity_Tesh/unity.h"
+
+void setUp(){}
+void tearDown(){}
+char dis()
+{
+    TEST_ASSERT_EQUAL_INT( Temp [20]);
+    TEST_ASSERT_EQUAL_INT( Temp [20]));
+    TEST_ASSERT_EQUAL_INT('Air_puritylevel',start[]);
+}
+return UNITY_END();
+}

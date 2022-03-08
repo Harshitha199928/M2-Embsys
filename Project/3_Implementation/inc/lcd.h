5b651465d7e8b5317e67823870f665c6633af243
Harshitha.N <55775183+Harshitha199928@users.noreply.github.com>
2022-03-08 09:57:29 +0530

    Update lcd.h

--- Project/3_Implementation/inc/lcd.h
+++ Project/3_Implementation/inc/lcd.h
@@ -5,13 +5,13 @@
 #define PORT_B PORTB			/* Define LCD data port */
 #define LCD_RS PB2				/* Define Register Select pin */
 #define LCD_EN PB3 				/* Define Enable signal pin */
-void LCD_CMD( unsigned char command );
-void LCD_CHAR_WISE( unsigned char ch );
-void LCD_INITIALIZATION(void);
-void LCD_DISPLAY (char *temperature_value);
-void CLEAR_LCD();
+void Lcd_cmd( unsigned char command );
+void Lcd_charwise( unsigned char ch );
+void Lcd_ini(void);
+void Lcd_dis (char *temperature_value);
+void Clear_lcd();
 
 void lcd(void);
 void InitADC(void);
 uint16_t ReadADC(uint8_t ch);
-#endif // _lcd_h_
+#endif
